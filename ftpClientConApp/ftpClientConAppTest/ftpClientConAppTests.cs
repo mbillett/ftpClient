eca377c3c0d5dc29973f8ebc6ba4b60093e04563
mbillett <52435703+mbillett@users.noreply.github.com>
2019-07-17 20:13:52 -0700

    Add files via upload
    3 more tests

--- ftpClientConApp/ftpClientConAppTest/ftpClientConAppTests.cs
+++ ftpClientConApp/ftpClientConAppTest/ftpClientConAppTests.cs
@@ -11,6 +11,9 @@ namespace ftpClientTests
         private const string USERNAME = "anonymous";
         private const string PASSWORD = "anonymous";
         private const string SERVERNAME = "ftp://speedtest.tele2.net";
+        private const string EMAILADDRESS = "nobody@nowhere.com";
+        private const string FILENAME = "SomeFile.txt";
+        private const string LOCALFILEPATH = "SomeLocalFilePath";
 
         [TestMethod]
         public void TestuserName()
@@ -35,6 +38,30 @@ namespace ftpClientTests
             MyConnection.ServerName = "ftp://speedtest.tele2.net";
 
             Assert.AreEqual(SERVERNAME, MyConnection.ServerName);
+        }
+        [TestMethod]
+        public void TestEmailAddress()
+        {
+            ftpClientConApp.ServerConnectionInformation MyConnection = new ftpClientConApp.ServerConnectionInformation();
+            MyConnection.EmailAddress = "nobody@nowhere.com";
+
+            Assert.AreEqual(EMAILADDRESS, MyConnection.EmailAddress);
+        }
+        [TestMethod]
+        public void TestFileName()
+        {
+            ftpClientConApp.ServerConnectionInformation MyConnection = new ftpClientConApp.ServerConnectionInformation();
+            MyConnection.FileName = "SomeFile.txt";
+
+            Assert.AreEqual(FILENAME, MyConnection.FileName);
+        }
+        [TestMethod]
+        public void TestLocalFilePath()
+        {
+            ftpClientConApp.ServerConnectionInformation MyConnection = new ftpClientConApp.ServerConnectionInformation();
+            MyConnection.LocalFilePath = "SomeLocalFilePath";
+
+            Assert.AreEqual(LOCALFILEPATH, MyConnection.LocalFilePath);
         }
     }
 }
