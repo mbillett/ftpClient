cebcc62fc2bac4fe8613de0c49dc5a521bed0f6a
Colin Gregory <cgregory1035@gmail.com>
2019-07-23 19:05:23 -0700

    Added a class ListDirectoryLocal

---
+++ ftpClientConApp/ftpClientConApp/ListDirectoryLocal.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ftpClientConApp
+{
+    class ListDirectoryLocal
+    {
+        public static void ListDirectory()
+        {
+            string dir = GetUserInputFileOrDirectory();
+            Console.WriteLine("\n");
+
+            if (File.Exists(dir))
+            {
+                ListFileLocal(dir);
+            }
+            else if (Directory.Exists(dir))
+            {
+                string[] files = Directory.GetFiles(dir);
+                foreach (string fileName in files)
+                {
+                    ListFileLocal(fileName);
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a valid file or directory", dir);
+            }
+
+        }
+        public static void ListFileLocal(string file)
+        {
+            FileInfo info = new FileInfo(file);
+            FileAttributes attributes = info.Attributes;
+            DateTime creationTime = info.CreationTime;
+            Console.WriteLine("{2:-15} {0:100}\n    {3:-15} {1:30}\n", file, creationTime.ToString("f"), "File:", "Date Created:");
+        }
+        public static string GetUserInputFileOrDirectory()
+        {
+            string dir = @"c:\";
+            bool found = false;
+
+            while (!found)
+            {
+                Console.WriteLine("Enter a directory");
+                dir = Console.ReadLine();
+                if (File.Exists(dir) || Directory.Exists(dir))
+                {
+                    found = true;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry that is not a directory or file\n");
+                }
+            }
+            return dir;
+        }
+    }
+}
